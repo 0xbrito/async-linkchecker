cdf6fd9352a11ba49c3ea9962f5e2c86f9e146e6
Diego Brito <84880186+0xbrito@users.noreply.github.com>
2026-02-14 18:23:09 -0300

    Parse URLs in input file (#2)
    * Parse args and read content file
    
    * Parse file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, fs, process};
+
+fn parse_urls(content: &str) -> Vec<&str> {
+    content
+        .lines()
+        .filter_map(|line| {
+            let trimmed = line.trim();
+            trimmed.strip_prefix("- ").map(|url| url.trim())
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let content = fs::read_to_string(file_path).unwrap_or_else(|e| {
+        eprintln!("Failed to read file: {e}");
+        process::exit(1);
+    });
+
+    let _urls = parse_urls(&content);
+
+    todo!();
 }
