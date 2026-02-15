850d6ef15958fd6e533ca57e4658bd14d8048a6b
Diego Brito <84880186+0xbrito@users.noreply.github.com>
2026-02-15 14:29:46 -0300

    Add integration tests

---
+++ tests/integration.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use std::process::Command;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, MockServer, ResponseTemplate};
+
+#[tokio::test]
+async fn extracts_title_from_succesful_res() {
+    let server = MockServer::start().await;
+
+    let body = "<html><title>Some Title</title></html>";
+
+    Mock::given(method("GET"))
+        .and(path("/"))
+        .respond_with(ResponseTemplate::new(200).set_body_string(body))
+        .mount(&server)
+        .await;
+
+    let url = server.uri();
+
+    let input_path = "/tmp/input_mock.md";
+    let _ = fs::write(input_path, format!("- {url}"));
+
+    Command::new("cargo")
+        .args(["run", "--", input_path])
+        .output()
+        .expect("failed to execute process");
+
+    let output = fs::read_to_string("output.md").unwrap();
+
+    assert_eq!(output.trim(), format!("- [Some Title]({url})"));
+}
